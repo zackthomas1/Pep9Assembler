61f5a5f992a641828e0a93fc805b113bcda7f96f
zackthomas1 <zackthomas13@gmail.com>
2021-08-29 16:39:59 -0700

    Move javafx directory into lib directory for cleaner project structure.

--- src/main/Pep9AssemblerApp.java
+++ src/main/Pep9AssemblerApp.java
@@ -7,20 +7,24 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 
 
 public class Pep9AssemblerApp extends Application{
 
-    @Override
+  public static void main(String[] args) throws Exception 
+  {
+  System.out.println("App::main-> Start App");
+  launch(args);     
+  }
+
+
+  @Override
 	public void start(Stage stage) throws Exception
 	{
     FXMLLoader loader = new FXMLLoader();
-    // loader.setLocation(new URL("file:///C:/Dev/Java/Pep9Assembler/src/main/resources/pep9Assembler_gui_v001.fxml"));
     loader.setLocation(getClass().getResource("resources/pep9Assembler_gui_v001.fxml"));
     Parent content = loader.load();
       
-    // VBox vbox = loader.<VBox>load();
     Scene scene = new Scene(content);
 
     stage.setTitle("Pep9 Assembler");
@@ -28,11 +32,4 @@ public class Pep9AssemblerApp extends Application{
     stage.show();
 
   }
-
-    public static void main(String[] args) throws Exception 
-    {
-		System.out.println("App::main-> Start App");
-		launch(args);     
-    }
-
 }
