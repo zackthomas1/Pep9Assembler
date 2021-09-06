97f1ea24186fde12566d5c2328a0e8dc9b63db70
zackthomas1 <zackthomas13@gmail.com>
2021-09-05 18:56:03 -0700

    Remove unused imports

--- src/main/parser/Translator.java
+++ src/main/parser/Translator.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javafx.scene.input.Mnemonic;
 import main.utility.Const;
 import main.utility.InBuffer;
 import main.utility.Util;
